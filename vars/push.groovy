a033a8125926ad0f00d13de98d44f0b39ad0f9ff
!~cloud champ <hitanshuy76@gmail.com>
2026-07-07 16:51:42 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,13 +1,13 @@
 def call(String CredId, String tag, String image){
   echo "pushing image to dockerhub started"
   withCredentials([usernamePassword(
-    credentialsId:${CredId}
+    credentialsId:"${CredId}"
     "usernameVariable":"dockerhubusername"
     "passwordVariable":"dockerhubpassword"
   )])
   sh '''
-  docker login -u ${dockerhubusername} -p ${dockerhubpassword}
-  docker image tag todo-app:latest ${dockerhubusername}/${image}:${tag}
-  docker push ${dockerusername}/${image}:${tag}
+  docker login -u "${dockerhubusername}" -p "${dockerhubpassword}"
+  docker image tag todo-app:latest "${dockerhubusername}/${image}:${tag}"
+  docker push "${dockerusername}/${image}:${tag}"
   '''
 }
