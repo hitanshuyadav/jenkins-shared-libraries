0c84ea38c125e1a7619aa4d80d8baa7259839ca3
!~cloud champ <hitanshuy76@gmail.com>
2026-07-07 16:50:50 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def call(String image, String tag, String Hport, String Sport){
 echo "deployement started"
 sh '''
-docker run -p ${Hport}:${Sport} ${image}:${tag}
+docker run -p "${Hport}:${Sport} ${image}:${tag}"
 '''
 }
